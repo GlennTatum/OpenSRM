1fb7c8dd27a51ac11bdba6df7f5edf772461fd8a
GlennTatum <glennetatum@gmail.com>
2025-02-23 10:34:54 -0500

    prod url

--- opensrm/frontend/src/app/backend.ts
+++ opensrm/frontend/src/app/backend.ts
@@ -1,1 +1,4 @@
-export const R: (param: string) => string = (param: string) => { return 'http://localhost:8000' + '/' + param};
+// export const R: (param: string) => string = (param: string) => { return 'http://localhost:8000' + '/' + param};
+
+// prod
+export const R: (param: string) => string = (param: string) => { return 'http://opensrm.duckdns.org' + '/' + param};
